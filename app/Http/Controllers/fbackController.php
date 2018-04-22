2d7c7e9b8e1d1eef4acdcabc2c60f8899bd47be5
spell1612 <spell1612@gmail.com>
2018-04-22 15:13:32 +0530

    first push

---
+++ app/Http/Controllers/fbackController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dhisha\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class fbackController extends Controller
+{
+    //
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dhisha\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'Dhisha\Model' => 'Dhisha\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
